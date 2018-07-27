2021fbd33762fdb415d843babf93f27889a98024
Andrew LeTourneau <andy@clockwork.com>
2018-07-27 17:12:29 -0500

    Made commands optional, and updated errors Updated README

--- README.md
+++ README.md
@@ -6,24 +6,43 @@ Conversion tool to assist creating Kubernetes Secrets YAML files by providing do
 
 ## How it Works & Usage
 
-`envtokube sample.env sample.yml`
-* Will convert `sample.env` to `sample.yml`
+`envtokube [--namespace=<name>] <input> [output]`
+* `--namespace=` is optional, and will default to the output filename (without file extension)
+* An .env file 'input' is required, as the source of secrets to encode for the YAML output file
+* If 'output' (a YAML file) is not provided, the input filename will be used, with a `.yml` extension
+
+`envtokube sample.env`
+* Will convert a `sample.env` to `sample.yml`
+* It will auto-set the metadata.name to be 'sample'
+* It will auto-set the metadata.namespace to be 'default'
+
+
+`envtokube sample.env out.yml`
+* Will convert `sample.env` to `out.yml`
+* It will auto-set the metadata.name to be 'out'
+* It will auto-set the metadata.namespace to be 'default'
+
+
+`envtokube --namespace=hello-world sample.env out.yml`
+* Will convert `sample.env` to `out.yml`
+* It will auto-set the metadata.name to be 'out'
+* It will set the metadata.namespace to be 'hello-world'
 
 
 ## ToDo
 
-* Input a namespace, instead of always defaulting to "default"
 * Reverse, to "decrypt" to an dotenv
 * Inherit namespace, if you're overwriting an existing file
 * Providing a warning if you're overwriting an existing file
 
 ## Additional Resources
 
+Based on: [kube-secrets a helpful utility for dealing with K8s Secretes](https://github.com/jengo/kube-secrets), huge thanks!
+
+
+* [The Go dotenv port (what I used to help build this)](https://github.com/joho/godotenv)
 * [The original Ruby dotenv](https://github.com/bkeepers/dotenv)
 * [The PHP dotenv port (why I built this)](https://github.com/vlucas/phpdotenv)
-* [The Go dotenv port (what I used to help build this)](https://github.com/joho/godotenv)
-* [kube-secrets a helpful utility for dealing with K8s Secretes](https://github.com/jengo/kube-secrets)
-  * Huge thanks, got me 90% of the way there for this tool
 
 ## License
 
--- envtokube.go
+++ envtokube.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-yaml/yaml"
 	"path/filepath"
 	"encoding/base64"
+	"os"
 )
 
 
@@ -27,34 +28,45 @@ type secrets struct {
 
 
 func main() {
+	namespacePtr := flag.String("namespace", "default", "Specifies the Namespace for the kube secrets file.")
 	flag.Parse()
 	action := flag.Args()
 
-	fmt.Println(action)
+	argCount := len(action)
 
+	var newFilename string
+	if argCount == 1 {
+		newFilename = extractName(action[0])
+	} else if argCount == 2 {
+		newFilename = extractName(action[1])
+	} else {
+		log.Fatal("At minimum, and .env file must be provided")
+		os.Exit(1)
+	}
+
+	// Load in the ENVs
 	var myEnv map[string]string
 	myEnv, err := godotenv.Read(action[0])
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
+		os.Exit(1)
 	}
 
-	createNewSecrets(action[1], myEnv)
+
+	createNewSecrets(newFilename, myEnv, *namespacePtr)
 }
 
 
-func createNewSecrets(filename string, keys map[string]string) error {
+func createNewSecrets(filename string, keys map[string]string, namespace string) error {
 	s            := secrets{}
 	s.APIVersion = "v1"
 	s.Kind       = "Secret"
+	s.Metadata.Namespace = namespace
 	s.Type       = "Opaque"
 	s.Data = make(map[string]string)
 
-	ext := filepath.Ext(filename)
-	// The secret name will have the directory and ext stripped
-	// /tmp/test.yml becomes test
-	s.Metadata.Name = filepath.Base(filename[0:len(filename)-len(ext)])
-
+	s.Metadata.Name = filename
 
 	for key, value := range keys {
 		updateValue := base64.StdEncoding.EncodeToString([]byte(value))
@@ -78,7 +90,16 @@ func (s *secrets) writeSecrets(filename string) error {
 		fmt.Printf("error: %v", err)
 	}
 
-	writeErr := ioutil.WriteFile(filename, newYML, 0644)
+	writeErr := ioutil.WriteFile(filename+".yml", newYML, 0644)
 
 	return writeErr
+}
+
+func extractName(filename string) string {
+	ext := filepath.Ext(filename)
+	// The secret name will have the directory and ext stripped
+	// /tmp/test.yml becomes test
+	name := filepath.Base(filename[0:len(filename)-len(ext)])
+
+	return name
 }
